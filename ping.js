5847931a701d83547d00c96e26bd608b4a2d60c7
paulsmee <paulsmee.is@gmail.com>
2019-04-24 14:02:36 +1000

    array changes

--- ping.js
+++ ping.js
@@ -20,14 +20,14 @@ let userIP = [
 ]
 
 // Obtain User IP Address
-function listUserIP(users) {
-    let userIPAddresses = []
-    for (let i=0; i<users.length; i+=1) {
-     userIPAddresses.push(users[i].ip)
-    }
-    return userIPAddresses
-}
-console.log(listUserIP(userIP))
+// function listUserIP(users) {
+//     let userIPAddresses = []
+//     for (let i=0; i<users.length; i+=1) {
+//      userIPAddresses.push(users[i].ip)
+//     }
+//     return userIPAddresses
+// }
+// console.log(listUserIP(userIP))
 
 // Obtain User Name (not used yet - hiding)
 // function listUserName(names) {
@@ -43,13 +43,56 @@ console.log(listUserIP(userIP))
 // Next we find out if the host is alive and place that into an array to use 
 // to call functions based on their alive status
 
-listUserIP(userIP).forEach(function (user) {
-    ping.sys.probe(user, function(isAlive){
-        user.status = isAlive;
-        console.log(userIP[0].status)
-        })
-    })
+// let's loop over the array directly
+// userIP.forEach(function (user) {
+//     ping.sys.probe(user.ip, function(isAlive){
+//         user.status = isAlive;   
+//     })        
+// })
 
+// the code above is "async" - so the log line below is called before it finishes
+// console.log(userIP);
+
+async function start() {
+    for (const user of userIP) {
+        console.log(`Starting to ping ${user.name}...`);
+        const resp = await ping.promise.probe(user.ip);
+        user.status = resp.alive;
+        console.log(`Finished pinging ${user.name}...`);
+    }
+}
+
+async function convertingCallbackToPromiseManually() {
+    for (const user of userIP) {
+        const isAlive = await new Promise((resolve) => {
+            ping.sys.probe(user.ip, function (isAlive) {
+                return resolve(isAlive);
+            })      
+        })
+        user.status = isAlive;
+    }    
+}
+
+async function allAtOnce() {
+    return Promise.all(userIP.map(async (user) => {
+        console.log(`Starting to ping ${user.name}...`)
+        const resp = await ping.promise.probe(user.ip);
+        user.status = resp.alive;
+        console.log(`Finished pinging ${user.name}...`)
+    }))
+}
+
+console.time('start')
+start().then(() => {
+    console.timeEnd('start')
+    console.log(userIP);
+});
+
+// start().then((val) => {
+//     console.log(val);
+// })
+
+//     console.log(userIP)
 // if (aliveStatus.forEach( function (aliveStatus) {
 // }) {
 //     userFound()
